e10a5ba4f44419dbed436c77100e48e78ef68830
7744Bhargavi <rohinipatil9712@gmail.com>
2025-08-08 00:09:55 +0530

    Create stockList.js

---
+++ ema21-screener/src/stockList.js
@@ -0,0 +1,14 @@
+const stockList = [
+  { symbol: "RELIANCE", exchange: "NSE" },
+  { symbol: "TCS", exchange: "NSE" },
+  { symbol: "INFY", exchange: "NSE" },
+  { symbol: "SBIN", exchange: "NSE" },
+  { symbol: "ICICIBANK", exchange: "NSE" },
+  { symbol: "RELIANCE", exchange: "BSE" },
+  { symbol: "TCS", exchange: "BSE" },
+  { symbol: "INFY", exchange: "BSE" },
+  { symbol: "SBIN", exchange: "BSE" },
+  { symbol: "ICICIBANK", exchange: "BSE" }
+];
+
+export default stockList;

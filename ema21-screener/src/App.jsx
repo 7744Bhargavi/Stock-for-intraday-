7997c6165f3a6771bfb05f15b4ad4726ef5dd20d
7744Bhargavi <rohinipatil9712@gmail.com>
2025-08-08 00:08:17 +0530

    Create App.jsx

---
+++ ema21-screener/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Screener from "./Screener";
+
+const App = () => {
+  return (
+    <div>
+      <h1>📊 Stock Screener - EMA 21</h1>
+      <Screener />
+    </div>
+  );
+};
+
+export default App;

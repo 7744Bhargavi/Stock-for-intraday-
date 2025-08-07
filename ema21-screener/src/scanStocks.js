ee1679e6e2bc8fb68b27e92730239dfebcb394d1
7744Bhargavi <rohinipatil9712@gmail.com>
2025-08-08 00:13:58 +0530

    Create scanStocks.js

---
+++ ema21-screener/src/scanStocks.js
@@ -0,0 +1,29 @@
+import getCandles from "./getCandles";
+import checkCrossover from "./checkCrossover";
+import stockList from "./stockList";
+
+const scanStocks = async () => {
+  const crossedStocks = [];
+
+  for (const stock of stockList) {
+    try {
+      const candles = await getCandles(stock.symbol, stock.exchange);
+      const result = checkCrossover(candles);
+
+      if (result) {
+        crossedStocks.push({
+          symbol: stock.symbol,
+          exchange: stock.exchange,
+          direction: result,
+        });
+      }
+    } catch (error) {
+      console.error(`Error scanning ${stock.symbol}:`, error.message);
+    }
+  }
+
+  return crossedStocks;
+};
+
+export default scanStocks;
+    
